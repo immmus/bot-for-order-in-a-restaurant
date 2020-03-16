1afb28f7487a886a41bf950dd32aafed8c1d05be
Ilya Ksenofontov <ilya.ksenofontov@ocrv.ru>
2020-03-16 18:59:26 +0300

    # small ref with initDb.sql

--- telegram-bot/src/main/resources/initDb.sql
+++ telegram-bot/src/main/resources/initDb.sql
@@ -1,12 +1,18 @@
+DROP TABLE IF EXISTS MENU;
 DROP TABLE IF EXISTS menu_positions;
 DROP SEQUENCE IF EXISTS menu_positions_seq;
+DROP TYPE IF EXISTS CATEGORY_TYPE;
 
 CREATE SEQUENCE menu_positions_seq START 100000;
+
+CREATE TYPE CATEGORY_TYPE AS ENUM ('FOOD', 'BAR', 'OTHER');
 
 CREATE TABLE menu_positions (
     id INTEGER PRIMARY KEY DEFAULT nextval('menu_positions_seq'),
     name TEXT NOT NULL,
-    price NUMERIC NOT NULL
+    price NUMERIC NOT NULL,
+    category CATEGORY_TYPE,
+    description TEXT NOT NULL
 );
 
 CREATE TABLE MENU (
